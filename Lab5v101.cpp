69359b63c5900df6dc4da57beb75d88a417dfa32
OdinetsDmitry <User@DESKTOP-8PU1PQQ>
2021-12-03 01:49:01 +0300

    add git

--- Lab5v101.cpp
+++ Lab5v101.cpp
@@ -15,156 +15,192 @@
 
 template<class T>
 class VectorT {
-	T* arr;
+	T* array;
 	int size;
-	int capacity;
+	int numbElements;
 
 public:
-	VectorT();									//конструктор
-	VectorT(int capacity);
-	VectorT(int capacity, const T& initial);
-	VectorT(const VectorT<T>& vec);
-	~VectorT();
-	void reserve(int capacity);
-	bool is_empty();
-	int my_size();
-	int my_capacity();
-	T* begin();
-	T* end();
-	void push_back(const T& value);
-	void pop_back();
-	void clear();
-	VectorT<T>& operator=(const VectorT<T>& vec);
-	T& operator[](int index);
+	VectorT();										//конструктор
+	VectorT(int numbElements);							//конструктор
+	VectorT(int numbElements, const T& elementInit);	//конструктор наполняет вектор элементами elementInit
+	VectorT(const VectorT<T>& vec);					//конструктор копирования
+	~VectorT();										//деструктор
+
+	void viewVectorT();								//выводэлементов вектора в консоль
+	void reserve(int numbElements);
+	int getSize();
+	int getNumbElements();
+	T* arrayBegin();								//начало вектора
+	T* arrayEnd();									//конец вектора
+	void pushBack(const T& value);					//добавляем элемент
+	void popBack();									//вытягиваем элемент 
+	void clear();									//очистка вектора
+	VectorT<T>& operator=(const VectorT<T>& vec);	//перегрузка оператора =
+	T& operator[](int index);						//перегрузка оператора []
 };
 template<class T>
-VectorT<T>::VectorT() {
-	arr = NULL;
+VectorT<T>::VectorT() 
+{
+	array = NULL;
 	size = 0;
-	capacity = 0;
+	numbElements = 0;
 }
 template<class T>
-VectorT<T>::VectorT(int capacity) {
+VectorT<T>::VectorT(int numbElements) 
+{
 	size = 0;
-	this->capacity = capacity;
-	arr = new T[capacity];
+	this->numbElements = numbElements;
+	array = new T[numbElements];
 }
 template<class T>
-VectorT<T>::VectorT(int capacity, const T& initial) {
-	this->capacity = capacity;
-	size = capacity;
-	arr = new T[capacity];
-	for (int i = 0; i < capacity; i++) {
-		arr[i] = initial;
+VectorT<T>::VectorT(int numbElements, const T& initial)
+{
+	this->numbElements = numbElements;
+	this->size = numbElements;
+	this->array = new T[numbElements];
+	for (int i = 0; i < numbElements; i++) 
+	{
+		array[i] = initial;
 	}
 }
 template<class T>
-VectorT<T>::VectorT(const VectorT& vec) {
+VectorT<T>::VectorT(const VectorT& vec)
+{
 	size = vec.size;
-	capacity = vec.capacity;
-	arr = new T[size];
-	for (int i = 0; i < size; i++) {
-		arr[i] = vec.arr[i];
+	numbElements = vec.numbElements;
+	array = new T[size];
+	for (int i = 0; i < size; i++) 
+	{
+		array[i] = vec.array[i];
 	}
 }
 template<class T>
-VectorT<T>::~VectorT() {
-	delete[]arr;
+VectorT<T>::~VectorT() 
+{
+	delete[]array;
 }
 template<class T>
-void VectorT<T>::reserve(int capacity) {
-	this->capacity = this->capacity + capacity;
-	T* newarr = new T[this->capacity];
-	for (int i = 0; i < size; i++) {
-		newarr[i] = arr[i];
+void VectorT<T>::viewVectorT()
+{
+	for (int i = 0; i < this->getSize(); i++)
+	{
+		std::cout << array[i] << "; ";
 	}
-	delete[]arr;
-	arr = newarr;
+}
+
+template<class T>
+void VectorT<T>::reserve(int numbElements) 
+{
+	this->numbElements = this->getNumbElements() + numbElements;
+	T* newarr = new T[this->numbElements];
+	for (int i = 0; i < this->getSize(); i++)
+	{
+		newarr[i] = array[i];
+	}
+	delete[]array;
+	array = newarr;
 }
 template<class T>
-bool VectorT<T>::is_empty() {
-	if (arr == NULL)
-		return true;
-	return false;
-}
-template<class T>
-int VectorT<T>::my_size() {
+int VectorT<T>::getSize() 
+{
 	return size;
 }
 template<class T>
-int VectorT<T>::my_capacity() {
-	return capacity;
+int VectorT<T>::getNumbElements() 
+{
+	return numbElements;
 }
 template<class T>
-T* VectorT<T>::begin() {
-	return arr;
+T* VectorT<T>::arrayBegin() 
+{
+	return array;
 }
 template<class T>
-T* VectorT<T>::end() {
-	return arr + my_capacity();
+T* VectorT<T>::arrayEnd() 
+{
+	return array + getNumbElements();
 }
 template<class T>
-void VectorT<T>::push_back(const T& value) {
-
-	if (size == capacity) {
-		reserve(5);
+void VectorT<T>::pushBack(const T& value) 
+{
+	if (size == numbElements) 
+	{
+		reserve(1);
 	}
-	arr[size++] = value;
+	array[size++] = value;
 }
 template<class T>
-void VectorT<T>::pop_back() {
-	if (size > 0) {
+void VectorT<T>::popBack() 
+{
+	if (size > 0) 
+	{
 		size--;
-		T* newarr = new T[capacity];
-		for (int i = 0; i < size; i++) {
-			newarr[i] = arr[i];
+		T* newarr = new T[numbElements];
+		for (int i = 0; i < size; i++)
+		{
+			newarr[i] = array[i];
 		}
-		delete[]arr;
-		arr = newarr;
+		delete[]array;
+		array = newarr;
 	}
-	else {
+	else 
+	{
 		std::cout << "You cant delete! Vector is empty" << std::endl;
 	}
 }
 template<class T>
-void VectorT<T>::clear() {
-	delete[] arr;
-	arr = NULL;
+void VectorT<T>::clear()
+{
+	delete[] array;
+	array = NULL;
 	size = 0;
-	capacity = 0;
+	numbElements = 0;
 }
 template<class T>
-VectorT<T>& VectorT<T>::operator=(const VectorT<T>& vec) {
-	delete[]arr;
+VectorT<T>& VectorT<T>::operator=(const VectorT<T>& vec) 
+{
+	delete[]array;
 	size = vec.size;
-	capacity = vec.capacity;
-	arr = new T[size];
-	for (int i = 0; i < size; i++) {
-		arr[i] = vec.arr[i];
+	numbElements = vec.numbElements;
+	array = new T[size];
+	for (int i = 0; i < size; i++) 
+	{
+		array[i] = vec.array[i];
 	}
 
 	return *this;
 }
 template<class T>
-T& VectorT<T>::operator[](int index) {
-	return arr[index];
+T& VectorT<T>::operator[](int index) 
+{
+	if (index < 0 || (index >= size)) index = 0;      // контроль за выходом из вектора 
+	return array[index];
 }
 
 
+int main() 
+{
 
+	VectorT<double> vec(3, 1.33);
+	vec.viewVectorT();
+	std::cout << std::endl;
+	vec.pushBack(2.6);
+	vec.viewVectorT();
+	std::cout << std::endl;
+	vec.popBack();
+	vec.viewVectorT();
+	std::cout << std::endl;
+	
+	VectorT<double> vec1(6, 12);
+	vec1.viewVectorT();
+	std::cout << std::endl;
 
-
-
-int main() {
-
-	VectorT<double> vec(10, 1);
-
-	VectorT<double> vec1(15, 12);
 	vec1 = vec;
-	vec.pop_back();
-	for (int i = 0; i < vec1.my_size(); i++) {
-		std::cout << vec1[i] << " ";
-	}
+	vec1.viewVectorT();
+	std::cout << std::endl;
+
+	vec.popBack();
+	vec.viewVectorT();	
 
 	return 0;
 }
